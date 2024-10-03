005e3addcfae0919b20afdd0d4dd618ecc679b55
farahmahfouz <farahmahfouz11@gmail.com>
2024-10-04 02:56:09 +0300

    Fix/bug in category schema

--- Validations/categoriesSchemas.js
+++ Validations/categoriesSchemas.js
@@ -1,8 +1,9 @@
-const joi = require("joi");
+const Joi = require("joi");
 
-const categorySchema = joi.object({
-    name: joi.string().required(),
-    description: joi.string().required().min(30).max(100),
+const categorySchema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required().min(30).max(100),
   });
 
-  module.exports ={ categorySchema };
+
+module.exports = categorySchema;
